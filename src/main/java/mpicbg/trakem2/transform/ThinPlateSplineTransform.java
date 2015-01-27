a6b462d4a4105fda94f232184a52dc37344b0526
Stephan Saalfeld <saalfeld@mpi-cbg.de>
2015-01-27 16:36:41 -0500

    fixed reading from dataString

--- src/main/java/mpicbg/trakem2/transform/ThinPlateSplineTransform.java
+++ src/main/java/mpicbg/trakem2/transform/ThinPlateSplineTransform.java
@@ -1,7 +1,7 @@
 package mpicbg.trakem2.transform;
 
+import ij.IJ;
 import jitk.spline.ThinPlateR2LogRSplineKernelTransform;
-import mpicbg.trakem2.transform.CoordinateTransform;
 
 public class ThinPlateSplineTransform implements CoordinateTransform {
 
@@ -10,13 +10,14 @@ public class ThinPlateSplineTransform implements CoordinateTransform {
 	public ThinPlateSplineTransform() {
 	}
 
-	public ThinPlateSplineTransform(ThinPlateR2LogRSplineKernelTransform tps) {
+	public ThinPlateSplineTransform(final ThinPlateR2LogRSplineKernelTransform tps) {
 		this.tps = tps;
 	}
 
-	public float[] apply(float[] location) {
+	@Override
+	public float[] apply(final float[] location) {
 
-		float[] out = new float[location.length];
+		final float[] out = new float[location.length];
 		for (int i = 0; i < location.length; i++) {
 			out[i] = location[i];
 		}
@@ -25,23 +26,27 @@ public class ThinPlateSplineTransform implements CoordinateTransform {
 		return out;
 	}
 
-	public void applyInPlace(float[] location) {
+	@Override
+	public void applyInPlace(final float[] location) {
 
 		tps.applyInPlace(location);
 	}
 
-	public void init(String data) throws NumberFormatException {
+	@Override
+	public void init(final String data) throws NumberFormatException {
 
 		final String[] fields = data.split("\\s+");
 
 		int i = 0;
 
-		int ndims = Integer.parseInt(fields[i++]);
-		int nLm = Integer.parseInt(fields[i++]);
+		IJ.log(fields[ i ]);
+
+		final int ndims = Integer.parseInt(fields[++i]);
+		final int nLm = Integer.parseInt(fields[++i]);
 
 		double[][] aMtx = null;
 		double[] bVec = null;
-		if (fields[i].equals("null") && fields[i + 1].equals("null")) {
+		if (fields[i + 1].equals("null") && fields[i + 2].equals("null")) {
 			// System.out.println(" No affines " );
 			i += 2;
 		} else {
@@ -50,34 +55,34 @@ public class ThinPlateSplineTransform implements CoordinateTransform {
 
 			for (int k = 0; k < ndims; k++)
 				for (int j = 0; j < ndims; j++) {
-					aMtx[k][j] = Double.parseDouble(fields[i++]);
+					aMtx[k][j] = Double.parseDouble(fields[++i]);
 				}
 			for (int j = 0; j < ndims; j++) {
-				bVec[j] = Double.parseDouble(fields[i++]);
+				bVec[j] = Double.parseDouble(fields[++i]);
 			}
 		}
 
 		// parse control points
-		double[][] srcPts = new double[ndims][nLm];
+		final double[][] srcPts = new double[ndims][nLm];
 		for (int l = 0; l < nLm; l++)
 			for (int d = 0; d < ndims; d++) {
-				srcPts[d][l] = Double.parseDouble(fields[i++]);
+				srcPts[d][l] = Double.parseDouble(fields[++i]);
 			}
 
 		// parse control point coordinates
 		int n = 0;
-		double[] dMtxDat = new double[nLm * ndims];
+		final double[] dMtxDat = new double[nLm * ndims];
 		for (int l = 0; l < nLm; l++)
 			for (int d = 0; d < ndims; d++) {
-				dMtxDat[n++] = Double.parseDouble(fields[i++]);
+				dMtxDat[n++] = Double.parseDouble(fields[++i]);
 			}
 
-		tps = new ThinPlateR2LogRSplineKernelTransform(srcPts, aMtx, bVec,
-				dMtxDat);
+		tps = new ThinPlateR2LogRSplineKernelTransform(srcPts, aMtx, bVec, dMtxDat);
 
 	}
 
-	public String toXML(String indent) {
+	@Override
+	public String toXML(final String indent) {
 		final StringBuilder xml = new StringBuilder();
 		xml.append(indent).append("<ict_transform class=\"")
 				.append(this.getClass().getCanonicalName())
@@ -86,23 +91,24 @@ public class ThinPlateSplineTransform implements CoordinateTransform {
 		return xml.append("\"/>").toString();
 	}
 
+	@Override
 	public String toDataString() {
 		final StringBuilder data = new StringBuilder();
 		toDataString(data);
 		return data.toString();
 	}
 
+	@Override
 	public CoordinateTransform copy() {
 		return new ThinPlateSplineTransform(tps);
 	}
 
 	private final void toDataString(final StringBuilder data) {
 
-		if (ThinPlateR2LogRSplineKernelTransform.class.isInstance(tps))
-			data.append("ThinPlateSplineR2LogR");
+		data.append("ThinPlateSplineR2LogR");
 
-		int ndims = tps.getNumDims();
-		int nLm = tps.getNumLandmarks();
+		final int ndims = tps.getNumDims();
+		final int nLm = tps.getNumLandmarks();
 
 		data.append(' ').append(ndims); // dimensions
 		data.append(' ').append(nLm); // landmarks
@@ -111,8 +117,8 @@ public class ThinPlateSplineTransform implements CoordinateTransform {
 			data.append(' ').append("null"); // aMatrix
 			data.append(' ').append("null"); // bVector
 		} else {
-			double[][] aMtx = tps.getAffine();
-			double[] bVec = tps.getTranslation();
+			final double[][] aMtx = tps.getAffine();
+			final double[] bVec = tps.getTranslation();
 			for (int i = 0; i < ndims; i++)
 				for (int j = 0; j < ndims; j++) {
 					data.append(' ').append(aMtx[i][j]);
@@ -123,13 +129,13 @@ public class ThinPlateSplineTransform implements CoordinateTransform {
 
 		}
 
-		double[][] srcPts = tps.getSourceLandmarks();
+		final double[][] srcPts = tps.getSourceLandmarks();
 		for (int l = 0; l < nLm; l++)
 			for (int d = 0; d < ndims; d++) {
 				data.append(' ').append(srcPts[d][l]);
 			}
 
-		double[] dMtxDat = tps.getKnotWeights();
+		final double[] dMtxDat = tps.getKnotWeights();
 		for (int i = 0; i < ndims * nLm; i++) {
 			data.append(' ').append(dMtxDat[i]);
 		}
